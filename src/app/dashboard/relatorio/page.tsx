e1e86216688188532b2bcd8ed16bf09b7213c2f7
AlanAkiyoshi <alanseigiakian@gmail.com>
2025-10-30 10:24:50 -0300

    depoy

--- src/app/dashboard/relatorio/page.tsx
+++
@@ -1,9 +0,0 @@
-const Page = () => {
-  return (
-    <main>
-      <h1>Relatório</h1>
-    </main>
-  );
-};
-
-export default Page;
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { neonConfig } from "@neondatabase/serverless";
-import ws from "ws";
 
-neonConfig.webSocketConstructor = ws;
+
 
 const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaNeon({ connectionString });
